a41d62d2b9e2c1cffca54eb4538df601ec0cb68f
Gr8-blip <greatheboss989@gmail.com>
2026-07-04 21:46:56 +0100

    add models admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Parent, Payment, Student, Family
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Parent)
+admin.site.register(Payment)
+admin.site.register(Student)
+admin.site.register(Family)
