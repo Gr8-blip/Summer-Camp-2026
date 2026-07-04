015f00307e8999b385429018e8c31bdc84ed65a9
Gr8-blip <greatheboss989@gmail.com>
2026-07-04 21:51:45 +0100

    update admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import User, Parent, Payment, Student, Family
 
 # Register your models here.
-admin.site.register(User)
 admin.site.register(Parent)
 admin.site.register(Payment)
 admin.site.register(Student)
